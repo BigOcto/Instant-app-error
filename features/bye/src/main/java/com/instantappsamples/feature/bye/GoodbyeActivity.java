76a39f03562e17f858d6cd922615d9a3a72a8c7e
zhangyu <zhangyu@qiyi.com>
2017-09-07 15:19:29 +0800

    Instant mode, android.permission.READ_PHONE_STATE requet permission, always return false

--- features/bye/src/main/java/com/instantappsamples/feature/bye/GoodbyeActivity.java
+++ features/bye/src/main/java/com/instantappsamples/feature/bye/GoodbyeActivity.java
@@ -16,7 +16,13 @@
 
 package com.instantappsamples.feature.bye;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Process;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -30,6 +36,8 @@ import com.instantappsamples.base.DebugBaseHelp;
 
 /** Simple activity that says goodbye. */
 public class GoodbyeActivity extends AppCompatActivity {
+  public static final String PERMISSION_READ_PHONE_STATE = "android.permission.READ_PHONE_STATE";
+  public static final int PERMISSION_ID_READ_PHONE_STATE = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +45,17 @@ public class GoodbyeActivity extends AppCompatActivity {
     int id = getResources().getIdentifier("activity_goodbye","layout",getApplication().getPackageName());
     Toast.makeText(this,String.valueOf(id),Toast.LENGTH_LONG).show();
     Log.e("getResource","layout getResource result id : " + id);
-//    setContentView(R.layout.activity_goodbye);
+    setContentView(R.layout.activity_goodbye);
 //    setContentView(id);
-    LayoutInflater inflate = LayoutInflater.from(this);
-    View view = inflate.inflate(id,null);
-    setContentView(view);
-
-    /*Using 'findViewById' get View */
-    int goodbye_text2_id = getResources().getIdentifier("goodbye_text2","id",this.getPackageName());
-    Log.e("getResource","textView getResource result id : " + goodbye_text2_id);
-    TextView goodbye_text2 = (TextView) view.findViewById(goodbye_text2_id);
-    goodbye_text2.setText("textView getResource with getIdentifier");
+//    LayoutInflater inflate = LayoutInflater.from(this);
+//    View view = inflate.inflate(id,null);
+//    setContentView(view);
+//
+//    /*Using 'findViewById' get View */
+//    int goodbye_text2_id = getResources().getIdentifier("goodbye_text2","id",this.getPackageName());
+//    Log.e("getResource","textView getResource result id : " + goodbye_text2_id);
+//    TextView goodbye_text2 = (TextView) view.findViewById(goodbye_text2_id);
+//    goodbye_text2.setText("textView getResource with getIdentifier");
 
     /* base library drawable*/
     ImageView imageView_library = (ImageView) findViewById(R.id.goodbye_img_left);
@@ -66,5 +74,36 @@ public class GoodbyeActivity extends AppCompatActivity {
     textView.setText(com.google.android.instantapps.samples.feature.base.R.string.hello_base_module);
 
 
+    /*Request permission*/
+    checkPermission(PERMISSION_READ_PHONE_STATE,PERMISSION_ID_READ_PHONE_STATE);
   }
+
+
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    boolean isGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+    Log.d("Permission", "onRequestPermissionsResult : " + isGranted);
+  }
+
+
+  public void checkPermission(String permission, int requestCode) {
+    if (!hasSelfPermission(this, permission)) {
+      Log.d("Permission", "hasSelfPermission : " + false);
+      String[] permisions = {permission};
+      ActivityCompat.requestPermissions(this, permisions, requestCode);
+    }
+  }
+
+  public static boolean hasSelfPermission(Context context, String permission) {
+    int hasPermission = context.checkPermission(permission,
+            android.os.Process.myPid(), Process.myUid()) ;
+    int secondPermission = ContextCompat.checkSelfPermission(context, permission);
+    int thirdPermission = PermissionChecker.checkCallingOrSelfPermission(context, permission);
+    int fourthPermission = PermissionChecker.checkSelfPermission(context, permission);
+    Log.d("Permission", "hasSelfPermission " + permission + ", hasPermission ="
+            + hasPermission + ", second=" + secondPermission + ",third=" + thirdPermission + ",fouth=" + fourthPermission);
+    return (fourthPermission == PackageManager.PERMISSION_GRANTED);
+  }
+
 }
